64b3d92d17163f881980115bf191ffc095b51548
Jovie Crammer <joviec31@gmail.com>
2026-07-16 16:52:31 +0100

    create note class to specify pitch and duration of each note

--- music/music_generator.py
+++ music/music_generator.py
@@ -1,17 +1,26 @@
+from music.note import Note
+
 
 def person_to_note(person, generation):
     notes = {
-        0: "c1",
-        1: "c2",
-        2: "c3",
-        3: "c4"
+        0: "C1",
+        1: "C2",
+        2: "C3",
+        3: "C4"
     }
-    return notes.get(generation)
+    pitch = notes.get(generation)
+    return Note(pitch, 1)
 
 
 def create_composition(root):
+    composition = []
+
     generations = root.get_generations()
     for generation_number, people in enumerate(generations):
         for person in people:
             note = person_to_note(person, generation_number)
-            print(f"{person.name} plays note {note}")
+            print(f"{person.name} plays {note}")
+            composition.append(note)
+
+    print(composition)
+    return composition
---
+++ music/note.py
@@ -0,0 +1,7 @@
+class Note:
+    def __init__(self, pitch, duration):
+        self.pitch = pitch
+        self.duration = duration
+
+    def __repr__(self):
+        return f"{self.pitch} ({self.duration}s)"
