d837c89af80ac8cb995f7d34dae0e2d279c0e117
Jovie Crammer <joviec31@gmail.com>
2026-07-18 21:19:36 +0100

    delete note definitions from midi writer

--- music/midi_writer.py
+++ music/midi_writer.py
@@ -1,51 +1,18 @@
 from midiutil import MIDIFile
+from music.note_numbers import NOTE_NUMBERS
+
 
 def write_midi(composition, filename="family.mid"):
     midi = MIDIFile(1)
-
     track = 0
     channel = 0
     time = 0
     tempo = 100
     midi.addTempo(track, time, tempo)
 
-    note_numbers = {
-        "C1": 24,
-        "D1": 26,
-        "E1": 28,
-        "F1": 29,
-        "G1": 31,
-        "A1": 33,
-        "B1": 35,
-
-        "C2": 36,
-        "D2": 38,
-        "E2": 40,
-        "F2": 41,
-        "G2": 43,
-        "A2": 45,
-        "B2": 47,
-
-        "C3": 48,
-        "D3": 50,
-        "E3": 52,
-        "F3": 53,
-        "G3": 55,
-        "A3": 57,
-        "B3": 59,
-
-        "C4": 60,
-        "D4": 62,
-        "E4": 64,
-        "F4": 65,
-        "G4": 67,
-        "A4": 69,
-        "B4": 71,
-    }
-
     for note in composition:
         print(note)
-        midi_note = note_numbers[note.pitch]
+        midi_note = NOTE_NUMBERS[note.pitch]
 
         midi.addNote(
             track,
